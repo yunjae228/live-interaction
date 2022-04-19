4c6b83f98e835eca3e8734b37e150b9c4174ec37
yunjae228 <yvkqzhs7@gmail.com>
2022-04-19 16:42:09 +0900

    프론트 UI with pug

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+// @ts-check
+
 const Koa = require('koa')
 const app = new Koa()
+const Pug = require('koa-pug')
+const path = require('path')
 
-app.use(async ctx => {
-    ctx.body = 'hello world'
+//@ts-ignore
+new Pug({
+  viewPath: path.resolve(__dirname, './views'),
+  app,
+})
+
+app.use(async (ctx) => {
+  await ctx.render('main')
 })
 
 app.listen(3000)
