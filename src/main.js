22d648b49af9060505049044ed7baf1766ae3e75
yunjae228 <yvkqzhs7@gmail.com>
2022-04-19 16:44:46 +0900

    koa-websocket 사용

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // @ts-check
 
 const Koa = require('koa')
-const app = new Koa()
+const app = websockify(new Koa())
 const Pug = require('koa-pug')
 const path = require('path')
+const route = require('koa-route')
+const websockify = require('koa-websocket')
 
 //@ts-ignore
 new Pug({
@@ -14,5 +16,25 @@ new Pug({
 app.use(async (ctx) => {
   await ctx.render('main')
 })
+
+// Regular middleware
+// Note it's app.ws.use and not app.use
+app.ws.use(function (ctx, next) {
+  // return `next` to pass the context (ctx) on to the next ws middleware
+  return next()
+})
+
+// Using routes
+app.ws.use(
+  route.all('/test/:id', function (ctx) {
+    // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
+    // the websocket is added to the context on `ctx.websocket`.
+    ctx.websocket.send('Hello World')
+    ctx.websocket.on('message', function (message) {
+      // do something with the message from client
+      console.log(message)
+    })
+  })
+)
 
 app.listen(3000)
